cda37a65be16b062ce46c95f47b0e03c133e96fc
peterszabo92 <peter.szabo13@gmail.com>
2024-03-12 23:19:55 +0100

    Premake fix

--- premake5.lua
+++ premake5.lua
@@ -1,121 +1,127 @@
-project "GLFW"
-	kind "StaticLib"
-	language "C"
-	staticruntime "off"
-	warnings "off"
+workspace "Moongoose"
+	architecture "x64"
 
-	staticruntime "on"
-	runtime "Release"
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "Moongoose/vendor/GLFW/include"
+
+include "Moongoose/vendor/GLFW"
+
+project "Moongoose"
+	location "Moongoose"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "mgpch.h"
+	pchsource "Moongoose/src/mgpch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links 
+	{ 
+		"GLFW",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"MG_PLATFORM_WINDOWS",
+			"MG_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Moongoose-Editor")
+		}
+
+	filter "configurations:Debug"
+		defines "MG_DEBUG"
+		buildoptions "/MDd"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "MG_RELEASE"
+		buildoptions "/MD"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MG_DIST"
+		buildoptions "/MD"
+		optimize "On"
+
+project "Moongoose-Editor"
+	location "Moongoose-Editor"
+	kind "ConsoleApp"
+	language "C++"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
-		"src/platform.c",
-		"src/vulkan.c",
-		"src/window.c",
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
 	}
 
-	filter "system:linux"
-		pic "On"
+	includedirs
+	{
+		"Moongoose/vendor/spdlog/include",
+		"Moongoose/src"
+	}
 
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_module.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
-	filter "system:macosx"
-		pic "On"
-
-		files
-		{
-			"src/cocoa_init.m",
-			"src/cocoa_monitor.m",
-			"src/cocoa_window.m",
-			"src/cocoa_joystick.m",
-			"src/cocoa_time.c",
-			"src/nsgl_context.m",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/osmesa_context.c",
-			"src/egl_context.c"
-		}
-
-		defines
-		{
-			"_GLFW_COCOA"
-		}
+	links
+	{
+		"Moongoose"
+	}
 
 	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
 		systemversion "latest"
 
-		files
+		defines
 		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_module.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
+			"MG_PLATFORM_WINDOWS"
 		}
 
 	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter { "system:windows", "configurations:Debug-AS" }	
-		runtime "Debug"
-		symbols "on"
-		sanitize { "Address" }
-		flags { "NoRuntimeChecks", "NoIncrementalLink" }
+		defines "MG_DEBUG"
+		buildoptions "/MDd"
+		symbols "On"
 
 	filter "configurations:Release"
-		runtime "Release"
-		optimize "speed"
+		defines "MG_RELEASE"
+		buildoptions "/MD"
+		optimize "On"
 
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "speed"
-        symbols "off"
+	filter "configurations:Dist"
+		defines "MG_DIST"
+		buildoptions "/MD"
+		optimize "On"
