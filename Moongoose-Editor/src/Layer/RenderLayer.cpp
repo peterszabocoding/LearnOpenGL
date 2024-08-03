496139804bb5aaa24c6351b929121d064c82e62b
peterszabo92 <peter.szabo13@gmail.com>
2024-08-03 16:46:31 +0200

    Fix main scene crash

--- Moongoose-Editor/src/Layer/RenderLayer.cpp
+++ Moongoose-Editor/src/Layer/RenderLayer.cpp
@@ -127,13 +127,6 @@ void RenderLayer::RenderToolbarMenu() const
 			{
 				const std::string worldFilePath = FileDialogs::OpenFile(".mgworld");
 				if (!worldFilePath.empty()) m_WorldManager->LoadWorld(worldFilePath);
-
-				Entity plane = m_WorldManager->GetLoadedWorld()->CreateEntity("Plane Mesh");
-
-
-				Ref<Mesh> planeMesh = QuadMeshRef(5.0f);
-				planeMesh->AddMaterial(m_AssetManager->GetDefaultAsset<Material>());
-				m_WorldManager->GetLoadedWorld()->AddComponent<MeshComponent>(plane, MeshComponent(planeMesh));
 			}
 
 			if (ImGui::MenuItem("Save World", "", false, m_WorldManager->IsWorldOpened()))
